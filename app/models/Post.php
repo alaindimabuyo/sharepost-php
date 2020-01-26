5b7bd748cea8305652b0375f880bdaf3284f815f
alainquanta <alain@quanta.com.ph>
2020-01-26 17:07:48 +0000

    edit post

--- app/models/Post.php
+++ app/models/Post.php
@@ -27,6 +27,21 @@
       $this->db->query('INSERT INTO posts (title, user_id, body) VALUES(:title, :user_id, :body)');
       // Bind values
       $this->db->bind(':title', $data['title']);
+      $this->db->bind(':id', $data['id']);
+      $this->db->bind(':body', $data['body']);
+
+      // Execute
+      if($this->db->execute()){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    public function updatePost($data){
+      $this->db->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+      // Bind values
+      $this->db->bind(':title', $data['title']);
       $this->db->bind(':user_id', $data['user_id']);
       $this->db->bind(':body', $data['body']);
 
